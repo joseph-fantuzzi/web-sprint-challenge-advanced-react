bf04d13ec5077766b63694e53890ba7212c3afd6
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-03-11 15:21:43 -0500

    implemented some tests

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,67 @@
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import AppFunctional from "./AppFunctional";
+
+beforeEach(() => {
+  render(<AppFunctional className="functional" />);
+});
+
+describe("App Functional Component", () => {
+  test("sanity", () => {
+    expect(true).toBe(false);
+  });
+
+  test("renders without errors", () => {
+    render(<AppFunctional className="functional" />);
+  });
+
+  test("renders the coordinates heading and submit button on the screen", () => {
+    const coordinatesHeading = screen.getByText("Coordinates (2, 2)");
+    expect(coordinatesHeading).toBeVisible();
+    expect(coordinatesHeading).toBeInTheDocument();
+    const submitBtn = screen.getByText("Submit");
+    expect(submitBtn).toBeVisible();
+    expect(submitBtn).toBeInTheDocument();
+  });
+
+  test("typing in the email input field changes the value of the input", () => {
+    const emailInput = screen.getByPlaceholderText("type email");
+    fireEvent.change(emailInput, { target: { value: "hello@gmail.com" } });
+    expect(emailInput).toHaveValue("hello@gmail.com");
+  });
+
+  test("up left down right reset buttons are visible on the screen", () => {
+    const leftBtn = screen.getByText("LEFT");
+    const upBtn = screen.getByText("UP");
+    const rightBtn = screen.getByText("RIGHT");
+    const downBtn = screen.getByText("DOWN");
+    const resetBtn = screen.getByText("reset");
+    expect(leftBtn).toBeVisible();
+    expect(leftBtn).toBeInTheDocument();
+    expect(upBtn).toBeVisible();
+    expect(upBtn).toBeInTheDocument();
+    expect(rightBtn).toBeVisible();
+    expect(rightBtn).toBeInTheDocument();
+    expect(downBtn).toBeVisible();
+    expect(downBtn).toBeInTheDocument();
+    expect(resetBtn).toBeVisible();
+    expect(resetBtn).toBeInTheDocument();
+  });
+
+  test("when clicking up it moves the coordinate heading to (2, 1)", () => {
+    const upBtn = screen.getByText("UP");
+    const coordinatesHeading = screen.getByText("Coordinates (2, 2)");
+    fireEvent.click(upBtn);
+    expect(coordinatesHeading.textContent).toBe("Coordinates (2, 1)");
+  });
+
+  test("when submitting an empty input, confirm the screen renders an error message", async () => {
+    const submitBtn = screen.getByText("Submit");
+    fireEvent.click(submitBtn);
+    const message = await screen.findByText("Ouch: email is required");
+    expect(message).toBeVisible();
+    expect(message).toBeInTheDocument();
+  });
+});
